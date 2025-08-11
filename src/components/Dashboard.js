b9488910a75feb5f7dda40e977d7ea904d033f29
Dinesh <mpsdinesh1221@gmail.com>
2025-08-11 12:11:34 +0530

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAICredentials } from "../utils/googleSheet";
+//import { getAICredentials } from "../utils/googleSheet";
 
 const Dashboard = () => {
   const [currentGame, setCurrentGame] = useState('');
